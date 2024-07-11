fb9959359fad5cc415c955314ce234c9edcd3534
Hamidovaa <hemidoffa55@gmail.com>
2024-07-11 14:34:45 +0400

    Development and Production profiles, brand,category,product,details added

---
+++ Core/AllAPI.Domain/Entities/Details.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AllAPI.Domain.Common;
+
+namespace AllAPI.Domain.Entities
+{
+    public class Details : EntityBase
+    {
+        public Details() 
+        { 
+
+        }
+        public Details(string title, string description, int categoryId) 
+        {
+            CategoryId = categoryId;
+            Title = title;
+            Description = description;
+        }
+        public required string Title { get; set; }
+        public required string Description { get; set; }
+        public required int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
